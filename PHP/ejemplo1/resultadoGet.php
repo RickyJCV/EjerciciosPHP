800826be1a290a7c8a03dfdac32dc209f42d7e45
Ricardo <a.ricardocabrera@gmail.com>
2019-09-26 02:06:19 +0200

    Añadidos comentarios

--- PHP/ejemplo1/resultadoGet.php
+++ PHP/ejemplo1/resultadoGet.php
@@ -1,5 +1,5 @@
  <?php 
-
+       //El get nos muestra la información y por tanto no la cifra
         $nombre = $_GET ["user"];
         $password = $_GET ["pass"];
         echo "<h1> Usuario: " . $nombre . "<br>" . "Password: " . $password . "</h1>";
