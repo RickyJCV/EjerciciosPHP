1c739e7f92d6dff4aa783de6e160937511c9f9a1
Ricardo <a.ricardocabrera@gmail.com>
2019-10-07 16:31:35 +0200

    nuevos

---
+++ PHP/entorno_servidor/OperadoresLogicos/Ejemplo.php
@@ -0,0 +1,21 @@
+<?php
+
+$A = 5;
+
+$B = 3;
+
+$C = -12;
+
+$result = ($A+$B == 8) || ($A-$B == 6);
+
+if ( $result == true) {
+
+echo "Se cumple la condición";
+
+}else {
+
+echo "No se cumple la condición";
+
+}
+
+?>
---
+++ PHP/entorno_servidor/OperadoresLogicos/Ejercicio1.php
@@ -0,0 +1,19 @@
+<?php
+
+$primerNumero = 8;
+$segundoNumero = 5;
+$resultado = 0;
+
+echo "A)<br>";
+$resultado = $primerNumero%5;
+echo "$resultado"."<br>";
+
+echo "B)<br>";
+$resultado = $primerNumero/$segundoNumero;
+echo "$resultado"."<br>";
+
+echo "C)<br>";
+$resultado = $primerNumero+$segundoNumero;
+echo "$resultado"."<br>";
+
+?>
