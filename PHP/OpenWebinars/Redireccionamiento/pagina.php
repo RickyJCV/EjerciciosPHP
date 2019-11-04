17c6278916b15817917814bb44115765ec290a62
Ricardo <a.ricardocabrera@gmail.com>
2019-11-04 14:04:58 +0100

    Redirecciones

---
+++ PHP/OpenWebinars/Redireccionamiento/pagina.php
@@ -0,0 +1,7 @@
+<?php
+//Viene de una redireccion
+//echo $_GET['nombre'];
+
+//Esto redirecciona de nuevo a otra pagina
+header("location:pagina2.php?name=Ricardo");
+?>
---
+++ PHP/OpenWebinars/Redireccionamiento/redirecciones.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <a href="pagina.php?nombre=ricky">Redireccion</a>
+</body>
+</html>
