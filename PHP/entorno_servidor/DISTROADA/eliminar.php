9ef2723c8d66bc0fbb843b6df880bd12d6b40f32
Ricardo <a.ricardocabrera@gmail.com>
2019-11-05 21:09:27 +0100

    5/11/19

---
+++ PHP/entorno_servidor/DISTROADA/eliminar.php
@@ -0,0 +1,18 @@
+<?php
+
+  
+  # Conexión a la base de datos
+    $conn = mysqli_connect( 'localhost', 'root', '', 'distroada');
+
+  # Sentencia para borrar registros de la base de datos
+  $delete = "delete from distros where id = '$_POST[id]'";
+
+  # ejecutamos la sentencia contra la base de datos PRUEBAS
+  $return = mysqli_query( $conn, $delete);
+
+  # Mostramos el resultado por pantalla
+  print_r( $return);
+
+  # Cerramos la conexión
+  mysqli_close( $conn);
+  ?>
