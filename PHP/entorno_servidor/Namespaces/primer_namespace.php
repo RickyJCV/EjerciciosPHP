84811583b8a0758d9e15493aad0e6dade122da59
Ricardo <a.ricardocabrera@gmail.com>
2019-11-25 18:47:23 +0100

    25/11/19

---
+++ PHP/entorno_servidor/Namespaces/primer_namespace.php
@@ -0,0 +1,12 @@
+<?php
+namespace Deswebcom;
+
+const PI = 3.14;
+function avisa(){
+    echo "Te estoy avisando";
+}
+class MiClase{
+    public function probando(){
+	echo "Esto es una prueba";
+    }
+}
